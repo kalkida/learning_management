1e9c5b1da6875c088730772c4390b5dc495fa127
X025me <exo25me@gmail.com>
2022-09-08 14:16:59 +0300

    local

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../user";
+import classReducer from "../class";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    class: classReducer,
   },
 });
 
