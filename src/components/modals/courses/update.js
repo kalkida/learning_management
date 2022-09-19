29117d7027c7f1a71349c006a3632721a19200a8
melaku <genuieman@gmail.com>
2022-09-19 16:48:18 +0300

    coures Ui with static

--- src/components/modals/courses/update.js
+++ src/components/modals/courses/update.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Select, Modal, message, TimePicker } from "antd";
+import { Form, Input, Button, Select, Modal, message, TimePicker, Tabs, Table } from "antd";
 import { useSelector } from "react-redux";
 import {
   doc,
@@ -18,156 +18,376 @@ import moment from "moment";
 
 const { Option } = Select;
 
-function Update({
+function UpdateCourse({
   handleCancel,
   openUpdate,
   data,
   updateComplete,
   setUpdateComplete,
 }) {
-  const uid = useSelector((state) => state.user.profile);
-  const [courses, setCourses] = useState({});
-  const [input, setInput] = useState([]);
-  const [classes, setClasses] = useState([]);
-  const [teachers, setTeachers] = useState([]);
-  const [subject, setSubject] = useState([]);
-  const [selectedSubject, setSelectedSubject] = useState("");
-  const [selectedLevel, setSelectedLevel] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [updateCourse, setUpdateCourse] = useState({
-    course_name: data.course_name,
-    teachers: data.teachers,
-    class: data.class,
-    schedule: data.schedule,
-    description: data.description,
-    school_id: data.school_id,
-  });
+  // const uid = useSelector((state) => state.user.profile);
+  // const [courses, setCourses] = useState({});
+  // const [input, setInput] = useState([]);
+  // const [classes, setClasses] = useState([]);
+  // const [teachers, setTeachers] = useState([]);
+  // const [subject, setSubject] = useState([]);
+  // const [selectedSubject, setSelectedSubject] = useState("");
+  // const [selectedLevel, setSelectedLevel] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // const [updateCourse, setUpdateCourse] = useState({
+  //   course_name: data.course_name,
+  //   teachers: data.teachers,
+  //   class: data.class,
+  //   schedule: data.schedule,
+  //   description: data.description,
+  //   school_id: data.school_id,
+  // });
   const days = ["Monday", "Thusday", "Wednsday", "Thursday", "Friday"];
 
-  useEffect(() => {
-    getCourseData();
-  }, []);
+  // useEffect(() => {
+  //   getCourseData();
+  // }, []);
 
-  const handleUpdate = async () => {
-    setLoading(true);
-    setDoc(doc(firestoreDb, "courses", data.key), updateCourse, { merge: true })
-      .then((response) => {
-        setLoading(false);
-        message.success("Data is updated successfuly");
-        setUpdateComplete(!updateComplete);
-        handleCancel();
-      })
-      .catch((error) => {
-        message.error("Data is not updated");
-        console.log(error);
-      });
-  };
+  // const handleUpdate = async () => {
+  //   setLoading(true);
+  //   setDoc(doc(firestoreDb, "courses", data.key), updateCourse, { merge: true })
+  //     .then((response) => {
+  //       setLoading(false);
+  //       message.success("Data is updated successfuly");
+  //       setUpdateComplete(!updateComplete);
+  //       handleCancel();
+  //     })
+  //     .catch((error) => {
+  //       message.error("Data is not updated");
+  //       console.log(error);
+  //     });
+  // };
 
-  const getCourseData = async () => {
-    const children = [];
-    const teachersArrary = [];
-    const subjectArrary = [];
-    const q = query(
-      collection(firestoreDb, "class"),
-      where("school_id", "==", uid.school)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      var datas = doc.data();
-      children.push({
-        ...datas,
-        key: doc.id,
-      });
-    });
-    const qTeachers = query(
-      collection(firestoreDb, "teachers"),
-      where("school_id", "==", uid.school)
-    );
-    const queryTeachers = await getDocs(qTeachers);
-    queryTeachers.forEach((doc) => {
-      var datas = doc.data();
-      teachersArrary.push({
-        ...datas,
-        key: doc.id,
-      });
-    });
-    const qSubject = query(
-      collection(firestoreDb, "subject"),
-      where("school_id", "==", uid.school)
-    );
-    const querySubject = await getDocs(qSubject);
-    querySubject.forEach((doc) => {
-      var datas = doc.data();
-      subjectArrary.push({
-        ...datas,
-        key: doc.id,
-      });
-    });
-    setClasses(children);
-    setTeachers(teachersArrary);
-    setSubject(subjectArrary);
-  };
+  // const getCourseData = async () => {
+  //   const children = [];
+  //   const teachersArrary = [];
+  //   const subjectArrary = [];
+  //   const q = query(
+  //     collection(firestoreDb, "class"),
+  //     where("school_id", "==", uid.school)
+  //   );
+  //   const querySnapshot = await getDocs(q);
+  //   querySnapshot.forEach((doc) => {
+  //     var datas = doc.data();
+  //     children.push({
+  //       ...datas,
+  //       key: doc.id,
+  //     });
+  //   });
+  //   const qTeachers = query(
+  //     collection(firestoreDb, "teachers"),
+  //     where("school_id", "==", uid.school)
+  //   );
+  //   const queryTeachers = await getDocs(qTeachers);
+  //   queryTeachers.forEach((doc) => {
+  //     var datas = doc.data();
+  //     teachersArrary.push({
+  //       ...datas,
+  //       key: doc.id,
+  //     });
+  //   });
+  //   const qSubject = query(
+  //     collection(firestoreDb, "subject"),
+  //     where("school_id", "==", uid.school)
+  //   );
+  //   const querySubject = await getDocs(qSubject);
+  //   querySubject.forEach((doc) => {
+  //     var datas = doc.data();
+  //     subjectArrary.push({
+  //       ...datas,
+  //       key: doc.id,
+  //     });
+  //   });
+  //   setClasses(children);
+  //   setTeachers(teachersArrary);
+  //   setSubject(subjectArrary);
+  // };
 
-  const handleCourse = (e) => {
-    setUpdateCourse({ ...updateCourse, [e.target.name]: e.target.value });
-  };
+  // const handleCourse = (e) => {
+  //   setUpdateCourse({ ...updateCourse, [e.target.name]: e.target.value });
+  // };
 
-  const handleClass = (value) => {
-    const classData = JSON.parse(value);
-    setSelectedLevel(classData.level + classData.section);
-    setUpdateCourse({ ...updateCourse, class: classData });
-  };
+  // const handleClass = (value) => {
+  //   const classData = JSON.parse(value);
+  //   setSelectedLevel(classData.level + classData.section);
+  //   setUpdateCourse({ ...updateCourse, class: classData });
+  // };
 
-  const handleSubject = (value) => {
-    setSelectedSubject(value);
-  };
+  // const handleSubject = (value) => {
+  //   setSelectedSubject(value);
+  // };
 
-  const getTeacherID = async (ID) => {
-    const docRef = doc(firestoreDb, "teachers", ID)
-    var data = "";
-    await getDoc(docRef).then(response => {
-      data = response.data();
-      data.key = response.id;
-    })
-    return data;
+  // const getTeacherID = async (ID) => {
+  //   const docRef = doc(firestoreDb, "teachers", ID)
+  //   var data = "";
+  //   await getDoc(docRef).then(response => {
+  //     data = response.data();
+  //     data.key = response.id;
+  //   })
+  //   return data;
+  // }
+
+  // const handleTeacher = (value) => {
+  //   const teacherdata = [];
+  //   value.map(async (item, i) => {
+  //     const respose = await getTeacherID(item);
+  //     teacherdata.push(respose);
+  //   });
+  //   setUpdateCourse({ ...updateCourse, teachers: teacherdata });
+
+  // };
+  // const handleScheduler = (value, i) => {
+  //   if (typeof value === "string") {
+  //     updateCourse.schedule[i].day = value;
+  //   } else {
+  //     const timeValue = [];
+  //     value.map((item, i) => {
+  //       timeValue.push(JSON.stringify(item._d));
+  //     });
+  //     updateCourse.schedule[i].day = value;
+  //   }
+  // };
+
+  // const handleNewScheduler = (value, i) => {
+  //   if (typeof value === "string") {
+  //     updateCourse.schedule[data.schedule.length + i].day = value;
+  //   } else {
+  //     const timeValue = [];
+  //     value.map((item, i) => {
+  //       timeValue.push(JSON.stringify(item._d));
+  //     });
+  //     updateCourse.schedule[data.schedule.length + i].time = timeValue;
+  //   }
+  // };
+
+  const Teacher = [{ name: "Teshome Belay" },
+  { name: "Kalkidan Hailu" }];
+
+  const columns = [
+    {
+      title: 'Teacher',
+      dataIndex: 'name',
+      key: 'name',
+    },
+
+  ];
+
+  const handleUpdate = () => {
+    console.log("update Data")
   }
-
-  const handleTeacher = (value) => {
-    const teacherdata = [];
-    value.map(async (item, i) => {
-      const respose = await getTeacherID(item);
-      teacherdata.push(respose);
-    });
-    setUpdateCourse({ ...updateCourse, teachers: teacherdata });
-
-  };
-  const handleScheduler = (value, i) => {
-    if (typeof value === "string") {
-      updateCourse.schedule[i].day = value;
-    } else {
-      const timeValue = [];
-      value.map((item, i) => {
-        timeValue.push(JSON.stringify(item._d));
-      });
-      updateCourse.schedule[i].day = value;
-    }
-  };
-
-  const handleNewScheduler = (value, i) => {
-    if (typeof value === "string") {
-      updateCourse.schedule[data.schedule.length + i].day = value;
-    } else {
-      const timeValue = [];
-      value.map((item, i) => {
-        timeValue.push(JSON.stringify(item._d));
-      });
-      updateCourse.schedule[data.schedule.length + i].time = timeValue;
-    }
-  };
 
   return (
     <div>
-      {data && openUpdate ? (
+      <div className="profile-header" >
+        <div className="course-avater" >
+          <img src="logo512.png" alt="profile" />
+          <div className="profile-info">
+            <h2>History 11A</h2>
+            <h3>Grade 11A</h3>
+          </div>
+        </div>
+        <div className="header-extra">
+          <div>
+            <h3>Assigned Teachers</h3>
+            <h4>2</h4>
+          </div>
+          <div>
+            <h3>Class/week</h3>
+            <h4>7</h4>
+          </div>
+        </div>
+      </div>
+      <div className="tab-content">
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Profile" key="1">
+            <Button onClick={handleUpdate}>Confirm</Button>
+            <div className="course-information">
+              <h1>Course Information</h1>
+              <div className="course-content">
+                <div className="course-category">
+                  <div>
+                    <span>Subject</span>
+                    <Select />
+                  </div>
+                  <div>
+                    <div>
+                      <span>Grade</span>
+                      <Select />
+                    </div>
+                    <div>
+                      <span>Section</span>
+                      <Select />
+                    </div>
+                  </div>
+                </div>
+                <div className="up-course-description">
+                  <h4>Coures Description</h4>
+                  <Input.TextArea
+                    width="100%"
+                    rows={6}
+                    defaultValue="course Description course Description"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="asssign-teacher">
+              <div className="assign-header">
+                <h4>Assigned Teachers</h4>
+                <Select width={"40%"}
+                  placeholder="Select Teachers"
+                />
+              </div>
+
+              <Table dataSource={Teacher} columns={columns} />
+            </div>
+            <div className="schedule">
+              <h4>Weekly Schedule</h4>
+              <div className="up-card-schedule">
+                <h2 >Class 7B</h2>
+                <div className="schedule-header">
+                  <div>
+                    <p> Period</p>
+                  </div>
+                  <div>
+                    <p> Start time</p>
+                    <p> End time</p>
+                  </div>
+                </div>
+
+                <Select
+                  style={{ width: "40%" }}
+                  placeholder="First Select Days"
+
+                >
+                  {days.map((item, index) => (
+                    <Option value={item} label={item}>
+                      {item}
+                    </Option>
+                  ))}
+                </Select>
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Select
+                  style={{ width: "40%" }}
+                  placeholder="First Select Days"
+
+                >
+                  {days.map((item, index) => (
+                    <Option value={item} label={item}>
+                      {item}
+                    </Option>
+                  ))}
+                </Select>
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Select
+                  style={{ width: "40%" }}
+                  placeholder="First Select Days"
+
+                >
+                  {days.map((item, index) => (
+                    <Option value={item} label={item}>
+                      {item}
+                    </Option>
+                  ))}
+                </Select>
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Select
+                  style={{ width: "40%" }}
+                  placeholder="First Select Days"
+
+                >
+                  {days.map((item, index) => (
+                    <Option value={item} label={item}>
+                      {item}
+                    </Option>
+                  ))}
+                </Select>
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Select
+                  style={{ width: "40%" }}
+                  placeholder="First Select Days"
+
+                >
+                  {days.map((item, index) => (
+                    <Option value={item} label={item}>
+                      {item}
+                    </Option>
+                  ))}
+                </Select>
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+
+              </div>
+            </div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Attendance" key="2">
+            Content of Tab Pane 2
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Assignment" key="3">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+      {/* {data && openUpdate ? (
         <Modal
           visible={openUpdate}
           title="Update Course"
@@ -366,9 +586,9 @@ function Update({
             </Form.Item>
           </Form>
         </Modal>
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
 
-export default Update;
+export default UpdateCourse;
--- src/components/modals/courses/view.js
+++ src/components/modals/courses/view.js
@@ -1,13 +1,160 @@
 import React, { useEffect, useState } from "react";
-import { Modal, Form, Input, Button, Select, TimePicker } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Modal, Form, Input, Button, Select, TimePicker, Tabs, Table } from "antd";
 import moment from "moment";
+import './style.css';
 
 const { Option } = Select;
 
-function View({ handleCancel, openView, data, coursedata, sectionData }) {
+function ViewCourse({ handleCancel, openView, data, coursedata, sectionData }) {
+
+  const navigate = useNavigate();
+  const Teacher = [{ name: "Teshome Belay" },
+  { name: "Kalkidan Hailu" }];
+
+  const columns = [
+    {
+      title: 'Teacher',
+      dataIndex: 'name',
+      key: 'name',
+    },
+
+  ];
+
+  const handleUpdate = () => {
+    console.log("navigation")
+    navigate('/update-course');
+  }
   return (
-    <>
-      {data && openView ? (
+    <div>
+      <div className="profile-header" >
+        <div className="course-avater" >
+          <img src="logo512.png" alt="profile" />
+          <div className="profile-info">
+            <h2>History 11A</h2>
+            <h3>Grade 11A</h3>
+          </div>
+        </div>
+        <div className="header-extra">
+          <div>
+            <h3>Assigned Teachers</h3>
+            <h4>2</h4>
+          </div>
+          <div>
+            <h3>Class/week</h3>
+            <h4>7</h4>
+          </div>
+        </div>
+      </div>
+      <div className="tab-content">
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Profile" key="1">
+            <Button onClick={handleUpdate}>Edit Course</Button>
+            <div className="course-description">
+              <h4>Coures Description</h4>
+              <Input.TextArea
+                width="100%"
+                rows={4}
+                defaultValue="course Description course Description"
+              />
+            </div>
+            <div className="asssign-teacher">
+              <h4>Assigned Teachers</h4>
+              <Table dataSource={Teacher} columns={columns} />
+            </div>
+            <div className="schedule">
+              <h4>Weekly Schedule</h4>
+              <div className="card-schedule">
+                <h2 >Class 7B</h2>
+                <div className="schedule-header">
+                  <div>
+                    <p> Period</p>
+                  </div>
+                  <div>
+                    <p> Start time</p>
+                    <p> End time</p>
+                  </div>
+                </div>
+
+                <Input value={"monday"} style={{ width: "40%" }} />
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Input value={"Thusday"} style={{ width: "40%" }} />
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Input value={"Wednsday"} style={{ width: "40%" }} />
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Input value={"Thursday"} style={{ width: "40%" }} />
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+                <Input value={"Friday"} style={{ width: "40%" }} />
+                <TimePicker.RangePicker
+                  style={{ width: "60%" }}
+                  use12Hours
+                  format={"hh:mm"}
+                  value={
+                    [
+                      moment("2020-03-09 13:00"),
+                      moment("2020-03-09 13:00"),
+                    ]
+
+                  }
+                />
+
+              </div>
+            </div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Attendance" key="2">
+            Content of Tab Pane 2
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Assignment" key="3">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+      {/* {data && openView ? (
         <Modal
           visible={openView}
           title="View Course"
@@ -85,9 +232,9 @@ function View({ handleCancel, openView, data, coursedata, sectionData }) {
             ) : null}
           </Form>
         </Modal>
-      ) : null}
-    </>
+      ) : null} */}
+    </div>
   );
 }
 
-export default View;
+export default ViewCourse;
