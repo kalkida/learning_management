e4ec1c1608e6daf4218c5ba8d53711733635f5c2
kale spencer <g5sectionf@gmail.com>
2022-09-22 19:27:47 +0300

    update 70%

--- src/components/modals/classes/update.js
+++ src/components/modals/classes/update.js
@@ -9,7 +9,9 @@ import {
     query,
     updateDoc,
     getDoc,
+    deleteDoc
 } from "firebase/firestore";
+import moment from "moment";
 import { firestoreDb, storage } from "../../../firebase";
 import { useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -24,7 +26,7 @@ const { Option } = Select;
 function UpdateClass() {
 
     const uid = useSelector((state) => state.user.profile);
-
+    const [input, setInput] = useState([]);
     const [loading, setLoading] = useState(false);
     const [courses, setcourse] = useState([]);
     const [students, setStudents] = useState([]);
@@ -40,6 +42,7 @@ function UpdateClass() {
         section: data.section,
         school_id: data.school_id,
         course: data.course,
+        schedule:[],
     });
 
     const [datas, setData] = useState([]);
@@ -49,12 +52,46 @@ function UpdateClass() {
   
     const navigate = useNavigate();
   
-    const dataschedule = [
-      {
+    const days = ["Monday", "Thusday", "Wednsday", "Thursday", "Friday"];
   
-      }
-    ]
+
+    const handleScheduler = (value, i) => {
+      // if (typeof value === "string") {
+      //   updateCourse.schedule[i].day = value;
+      // } else {
+      //   const timeValue = [];
+      //   value.map((item, i) => {
+      //     timeValue.push(JSON.stringify(item._d));
+      //   });
+      //   updateCourse.schedule[i].day = timeValue;
+      // }
+    };
   
+    const handleNewScheduler = (value, i) => {
+      // if (typeof value === "string") {
+      //   updateCourse.schedule[data.schedule.length + i].day = value;
+      // } else {
+      //   const timeValue = [];
+      //   value.map((item, i) => {
+      //     timeValue.push(JSON.stringify(item._d));
+      //   });
+      //   updateCourse.schedule[data.schedule.length + i].time = timeValue;
+      // }
+    };
+
+    const handleDelete = () => {
+      deleteDoc(doc(firestoreDb, "class", data.key), updateClass)
+        .then((response) => {
+          setLoading(false);
+          message.success("Data is Deleted successfuly");
+          navigate('/list-class')
+  
+        })
+        .catch((error) => {
+          message.error("Data is not Deleted, Try Again");
+          console.log(error);
+        });
+    }
     const scheduleColumn =[
       {
         title: 'Period',
@@ -97,7 +134,6 @@ function UpdateClass() {
         title: 'First Name',
         dataIndex: 'first_name',
         key: 'first_name'
-  
   
       },
       {
@@ -183,6 +219,84 @@ function UpdateClass() {
         // }
     };
 
+    const getSchool = async () => {
+      const docRef = doc(firestoreDb, "schools", uid.school);
+      const docSnap = await getDoc(docRef);
+  
+      if (docSnap.exists()) {
+        var dataset = docSnap.data();
+        return dataset;
+      } else {
+      }
+    };
+      
+  const getClassData = async (ID) => {
+    const docRef = doc(firestoreDb, "class", ID)
+    
+    var data = "";
+    await getDoc(docRef).then(response => {
+      data = response.data();
+      data.key = response.id;
+    })
+    return data;
+  }
+
+  const getSubjectData = async (ID) => {
+    const docRef = doc(firestoreDb, "subject", ID)
+    var data = "";
+    await getDoc(docRef).then(response => {
+      data = response.data();
+      data.key = response.id;
+    })
+    return data;
+  }
+
+  const getTeacherData = async (ID) => {
+    const docRef = doc(firestoreDb, "teachers", ID)
+    var data = "";
+    await getDoc(docRef).then(response => {
+      data = response.data();
+      data.key = response.id;
+    })
+    return data;
+  }
+  const getData = async (data) => {
+
+    data.class = await getClassData(data.class)
+    data.subject = await getSubjectData(data.subject)
+
+    data.teachers?.map(async (item, index) => {
+      data.teachers[index] = await getTeacherData(item)
+    })
+    return data;
+  }
+
+
+    const getCourse = async (course) =>{
+      var branches = await getSchool();
+      const q = query(
+        collection(firestoreDb, "courses"),
+        where("school_id", "in", branches.branches));
+      var temporary = [];
+      const snap = await getDocs(q);
+      console.log("dataaa     ",data.course.length)
+  for (var i = 0; i < course.length; i++) {
+      snap.forEach(async (doc) => {
+        var datause = doc.data();
+        datause.key = doc.id;
+      if(datause.key == course[i]) {
+        console.log("==============================")
+        console.log(course[i]);
+        console.log("==============================")
+        getData(datause).then(response => temporary.push(response))
+      }
+      });
+  }
+      setTimeout(() => {
+        setData(temporary);
+      }, 2000);
+    }
+
     const getStudents = async (level) => {
         const children = [];
         const q = query(
@@ -200,6 +314,7 @@ function UpdateClass() {
         });
         setStudents(children);
     };
+
     const getClass = async () => {
         const children = [];
         const sectionArray = [];
@@ -244,19 +359,25 @@ function UpdateClass() {
         return data;
 
     }
-
-
-
-
     useEffect(() => {
         getClass();
         getStudents(data.level);
+        getCourse(data.course);
     }, []);
 
     const handleClass = (e) => {
         if (e.target.name === "level") {
             getStudents(e.target.value);
         }
+    }
+
+    const handleCoursse = (value) =>{
+      value.map(async (item, i) => {
+        const response = await getStudentID(item)
+        value[i] = response;
+    });
+    setUpdateClass({ ...updateClass, student: value });
+
     }
     const handleStudent = (value) => {
         value.map(async (item, i) => {
@@ -293,17 +414,137 @@ function UpdateClass() {
             <Tabs.TabPane tab="Profile" key="1">
               <Button className="btn-confirm" onClick={handleUpdate}>Edit Class</Button>
               <div className="asssign-teacher">
-                <h4>Assigned Students</h4>
+              <div className="assign-header">
+                <h1  style={{
+                  fontSize:24,
+                  fontWeight: "bold",
+                }} >Assigned Students</h1>
+                         <Select
+                                placeholder="select Students"
+                                onChange={handleStudent}
+                                defaultValue={data.student}
+                                optionLabelProp="label"
+                                mode="multiple"
+                                maxTagCount={2}
+                            >
+                                {students.map((item, index) => (
+                                    <Option value={item.key} label={item.first_name + " " + (item.last_name ? item.last_name : "")}>
+                                        {item.first_name + " " + (item.last_name ? item.last_name : "")}
+                                    </Option>
+                                ))}
+                            </Select>
+                            </div>
                 <Table dataSource={data.student} columns={columns} />
+              
               </div>
               <div className="asssign-teacher">
-                <h4>Assigned Courses</h4>
+              <div className="assign-header">
+              <h1  style={{
+                  fontSize:24,
+                  fontWeight: "bold",
+                }} >Assigned Courses</h1>
+                 <Select
+                                placeholder="select Students"
+                                onChange={handleStudent}
+                                defaultValue={data.student}
+                                optionLabelProp="label"
+                                mode="multiple"
+                                maxTagCount={2}
+                            >
+                                {students.map((item, index) => (
+                                    <Option value={item.key} label={item.first_name + " " + (item.last_name ? item.last_name : "")}>
+                                        {item.first_name + " " + (item.last_name ? item.last_name : "")}
+                                    </Option>
+                                ))}
+                            </Select>
+                    </div>    
                 <Table dataSource={datas} columns={courseColumns} />
               </div>
-              <div className="asssign-teacher">
-                <h4>Weekly Schedule</h4>
-                <Table dataSource={datas} columns={scheduleColumn} />
+              <div className="schedule">
+              <h1  style={{
+                  fontSize:24,
+                  fontWeight: "bold",
+                }} >Weekly Schedule</h1>
+              <div className="up-card-schedule">
+                <h2 >Class {updateClass.class ? updateClass.class.level + updateClass.class.section : null}</h2>
+                <div className="schedule-header">
+                  <div>
+                    <p> Period</p>
+                  </div>
+                  <div>
+                    <p> Start time</p>
+                    <p> End time</p>
+                  </div>
+                </div>
+
+                {data.schedule?.map((item, i) => (
+                  <>
+                    <Select
+                      style={{ width: "40%" }}
+                      placeholder="First Select Days"
+                      onChange={(e) => handleScheduler(e, i)}
+                      defaultValue={item.day}
+                      in
+                    >
+                      {days.map((item, index) => (
+                        <Option key={index} value={item} label={item}>
+                          {item}
+                        </Option>
+                      ))}
+                    </Select>
+                    <TimePicker.RangePicker
+                      style={{ width: "60%" }}
+                      format={"hh:mm"}
+                      use12Hours
+                      defaultValue={
+                        item.time.length
+                          ? [
+                            moment(JSON.parse(item.time[0])),
+                            moment(JSON.parse(item.time[1])),
+                          ]
+                          : []
+                      }
+                      onChange={(e) => handleScheduler(e, i)}
+                    />
+                  </>
+                ))}
+                {input.map((item, i) => (
+                  <>
+                    <Select
+                      style={{ width: "40%" }}
+                      placeholder="First Select Days"
+                      onChange={(e) => handleNewScheduler(e, i)}
+                    >
+                      {days.map((item, index) => (
+                        <Option key={index} value={item} label={item}>
+                          {item}
+                        </Option>
+                      ))}
+                    </Select>
+                    <TimePicker.RangePicker
+                      style={{ width: "60%" }}
+                      format={"hh:mm"}
+                      use12Hours
+                      onChange={(e) => handleNewScheduler(e, i)}
+                    />
+                  </>
+                ))}
+                <Button
+                  style={{ float: "right" }}
+                  onClick={() => {
+                    setInput([...input, 0]);
+                    setUpdateClass({
+                      ...updateClass,
+                      schedule: [...updateClass.schedule, { day: "", time: [] }],
+                    });
+                  }}
+                >
+                  Add New
+                </Button>
+
               </div>
+            </div>
+            <Button className="btn-dlt" type="primary" danger onClick={handleDelete}>Delete</Button>
 
               </Tabs.TabPane>
           <Tabs.TabPane tab="Attendance" key="2">
