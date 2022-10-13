c18bb385a28a3f94c8c0258456d45e4ce3b2f658
kale spencer <g5sectionf@gmail.com>
2022-10-13 13:14:43 +0300

    student some edit

--- src/components/modals/student/Update.js
+++ src/components/modals/student/Update.js
@@ -267,21 +267,23 @@ function UpdateStudents() {
       <div className="bg-[#F9FAFB] h-[100vh] p-2 -mt-14">
         <div className="add-header mb-6 items-center">
           < h1 className="text-[1.5rem] font-jakarta" > Edit Student</h1 >
-          <button onClick={async () => await updateStudent()}>
-            <FontAwesomeIcon className="mr-2" icon={faCheck} />Confirm
+          <button 
+          className="btn-confirm  bg-[#E7752B]"
+          onClick={async () => await updateStudent()}>
+            <FontAwesomeIcon className="mr-2 text-[#FFF]" icon={faCheck} />Confirm
           </button>
         </div >
 
         <div className="tab-content">
           <Tabs defaultActiveKey="1">
             <Tabs.TabPane tab={
-              <p className="text-xl font-bold text-center ml-5 font-jakarta">Profile</p>
+              <p className="text-base font-bold text-center ml-5 font-jakarta">Profile</p>
             } key="1">
 
               <div className="add-teacher">
                 <div className="avater-img">
                   <div>
-                    <h2>Student Picture</h2>
+                    <h2 className="text-[#475467] text-sm font-jakarta">Student Picture</h2>
                     <img src={file ? URL.createObjectURL(file) : data.avater ? data.avater : "img-5.jpg"} />
                   </div>
                   <div className="file-content">
